403677e05d9cdb501c5895a6e47b05bc405c5e57
gil-gomes <gilanderson98@outlook.com>
2023-11-24 16:01:51 -0300

    Teste de html e php

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,20 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
-?>
+$is_valid = true;
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+
+  if (empty($name)) {
+    $is_valid = false;
+    echo "<p>Ops. O campo nome está vazio!</p>";
+    return;
+  }
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "<p>Ops. O email digitado não é válido!</p>";
+    return;
+  }
+
+  echo "Seu nome é: " . $name . "<br>";
+  echo "Seu email é: " . $email;
+}
