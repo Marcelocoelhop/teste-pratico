9c6d938429642642e3cefe57e45421adcaa31071
lucas <lucaswtds@gmail.com>
2023-11-24 18:59:31 -0300

    Revert "feat: teste pratico em HTML e PHP"
    This reverts commit 7f8947eedcaab967812fce4d71616f8f9dc7bddf.

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,18 +1,3 @@
 <?php
-
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-
-  if ($name == "") {
-    exit('O campo nome está vazio.');
-  }
-
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    exit('O endereço de email não é válido.');
-  }
-
-  echo 'Nome recebido:' . $name;
-  echo '</br>';
-  echo 'E-mail recebido:' . $email;
-
+  // SEU CÓDIGO PHP AQUI
 ?>
