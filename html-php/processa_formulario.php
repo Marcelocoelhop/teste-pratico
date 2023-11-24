964d61662561d29c90d4cec8daec22c1ad39c7e5
Oivlisfriend <robertomarcelogongo@gmail.com>
2023-11-24 23:03:51 +0100

    feat: Minha nova feature

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,11 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+$nome = filter_input(INPUT_POST, "nome");
+$email = filter_input(INPUT_POST, "email");
+$senha = filter_input(INPUT_POST, "senha");
+echo "O nome digitado é " . $nome . "<br/> O email digitado é " . $email;
+if (strlen($nome))
+  echo "<br/> O campo nome não está vázio";
+if (filter_var($email, FILTER_VALIDATE_EMAIL))
+  echo "<br/> O email é valido";
+
 ?>
