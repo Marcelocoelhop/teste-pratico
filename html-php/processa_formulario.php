babb5fb968f3eb41582e4b922b0bb310e7fdf68f
lucas <lucaswtds@gmail.com>
2023-11-24 19:07:30 -0300

    feat: teste pratico em HTML e PHP

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,18 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+
+  if ($name == "") {
+    exit('O campo nome está vazio.');
+  }
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    exit('O endereço de email não é válido.');
+  }
+
+  echo 'Nome recebido:' . $name;
+  echo '</br>';
+  echo 'E-mail recebido:' . $email;
+
 ?>
