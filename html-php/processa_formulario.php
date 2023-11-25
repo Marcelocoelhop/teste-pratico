a4a17cca59a60b6091e374d678d19988f8c8dc93
Gustavo <gaudereto.sena@gmail.com>
2023-11-25 13:44:48 -0300

    feat: solução do teste prático

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,34 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+// SEU CÓDIGO PHP AQUI
+
+$errorMessages = array();
+if (isset($_POST['name']) && !empty($_POST['name']))  $name = addslashes($_POST['name']);
+else $errorMessages[] = "Campo nome vazio!";
+
+
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+  $isEmailValid = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+  if ($isEmailValid) $email = addslashes($_POST['email']);
+  else $errorMessages[] = "Email inválido!";
+} else {
+  $errorMessages[] = "Campo email vazio!";
+}
+
+if (isset($_POST['password']) && !empty($_POST['password'])) $senha = addslashes($_POST['password']);
+else $errorMessages[] = "Campo senha vazio!";
+
 ?>
+<link href="./css/style.css" rel="stylesheet">
+
+<div class="container gap-0">
+  <?php if (!empty($errorMessages) && isset($errorMessages)) { ?>
+    <?php foreach ($errorMessages as $message) { ?>
+      <p class='error-message'><span class="bold">ERROR:</span> <?= $message ?></p>
+    <?php } ?>
+  <?php } else { ?>
+    <p class='info'><span class="bold">Nome:</span> <?= $name ?></p>
+    <p class='info'><span class="bold">Email:</span> <?= $email ?></p>
+  <?php } ?>
+
+  <a class="button" href="./formulario.html">Voltar</a>
+</div>
--- next-react/pages/index.js
+++ next-react/pages/index.js
@@ -1,27 +1,35 @@
 import { useState } from "react";
-
 export default function Contador() {
   const [count, setCount] = useState(0);
 
-  const increment = () => {};
+  const increment = () => {
+    setCount(count + 1)
+     // setCount(previous => previous + 1) // Método alternativo
+  };
 
-  const decrement = () => {};
+  const decrement = () => {
+    setCount(count - 1)
+    // setCount(previous => previous - 1) // Método alternativo
+  };
 
   return (
-    <div>
-      <h1>0</h1>
-      <button
-        type='button'
-        onClick={increment}
-      >
-        Incrementar
-      </button>
-      <button
-        type='button'
-        onClick={decrement}
-      >
-        Decrementar
-      </button>
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="flex justify-between gap-2">
+        <button
+          type='button'
+          onClick={increment}
+        >
+          Incrementar
+        </button>
+        <button
+          type='button'
+          onClick={decrement}
+        >
+          Decrementar
+        </button>
+
+      </div>
     </div>
   );
 }
