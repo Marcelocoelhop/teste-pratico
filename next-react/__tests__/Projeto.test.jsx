3bb7488b38b0365455dc1fba8d49ddeb47e73187
rafaelvilaveras <rafaelvilaveras20@gmail.com>
2023-11-26 15:36:56 -0300

    Feat: Teste Prático em Next.JS/React com Hooks

---
+++ next-react/__tests__/Projeto.test.jsx
@@ -0,0 +1,19 @@
+// Contador.test.js
+import Contador from '@/pages';
+import { render, fireEvent } from '@testing-library/react';
+
+test('increments and decrements count correctly', () => {
+  const { getByText } = render(<Contador />);
+  const incrementButton = getByText('Incrementar');
+  const decrementButton = getByText('Decrementar');
+  const countDisplay = getByText('0');
+
+  fireEvent.click(incrementButton);
+  expect(countDisplay).toHaveTextContent('1');
+
+  fireEvent.click(decrementButton);
+  expect(countDisplay).toHaveTextContent('0');
+
+  fireEvent.click(decrementButton);
+  expect(countDisplay).toHaveTextContent('0');
+});
