1cf3094d0ca742de26358b0f082556c5b980556f
Juan Carlos Silva <juancarlossilva@MacBook-Pro-de-Juan.local>
2023-11-26 20:54:27 -0300

    feat: Minha nova feature

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,28 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+
+  if(!$_POST){ $_POST = json_decode(file_get_contents('php://input'), true); }
+
+  $nome = $_POST["nome"];
+  $email = $_POST["email"];
+  $senha = $_POST["senha"];
+
+  $erros = [];
+
+  if( empty($nome) ){
+    $erros[] = "O campo nome não pode estar vazio.";
+  }
+  if( strlen($email) < 8 || !strstr($email,'@') ){
+    $erros[] = "E-mail digitado incorretamente.";
+  }
+  if( strlen($senha) < 3 ){
+    $erros[] = "A senha deve possuir ao menos 3 caracteres";
+  }
+
+  if($erros) echo json_encode($erros);
+  else{
+    echo json_encode(['nome' => $nome,'email'=>$email]);
+  }
+  
+  
+  
 ?>
--- next-react/pages/index.js
+++ next-react/pages/index.js
@@ -3,25 +3,30 @@ import { useState } from "react";
 export default function Contador() {
   const [count, setCount] = useState(0);
 
-  const increment = () => {};
+  const increment = () => {setCount(count+1)};
 
-  const decrement = () => {};
+  const decrement = () => {setCount(count-1)};
 
   return (
-    <div>
-      <h1>0</h1>
-      <button
-        type='button'
-        onClick={increment}
-      >
-        Incrementar
-      </button>
-      <button
-        type='button'
-        onClick={decrement}
-      >
-        Decrementar
-      </button>
+    <div class="container">
+      <h1 class="header">{count}</h1>
+      <div class="middle">
+          <button
+            type='button'
+            class="button"
+            onClick={increment}
+          >
+            Incrementar
+          </button>
+          <button
+            type='button'
+            class="button"
+            onClick={decrement}
+          >
+            Decrementar
+          </button>
+
+      </div>
     </div>
   );
 }
