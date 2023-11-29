625482e2dcf6e83bbec6b55c37123d676ce1c414
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-11-29 16:49:09 -0300

    Implementa checagem básica no teste html-php

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,10 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+if (isset($_GET)) {
+	if (!isset($_GET['nome']) || $_GET['nome'] == '') {
+		echo '<h1>Nome não pode ser vazio</h1>';
+	}
+	else if (!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) {
+		echo '<h1>Email inválido</h1>';
+	}
+}
 ?>
