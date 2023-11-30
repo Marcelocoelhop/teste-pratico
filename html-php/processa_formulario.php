94df85f31b4ee41b4000b3d208c4f1af02caa917
Paulo Ricardo <prmaltaleal@gmail.com>
2023-11-29 23:57:56 -0300

    feat: Minha nova feature

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,30 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $senha = $_POST['senha'];
+
+  $validacoes = array();
+
+  if (empty($nome)) {
+    $validacoes[] = "Nome obrigatório.";
+  }
+
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $validacoes[] = "Informe um endereço de email válido.";
+  }
+
+  if (empty($validacoes)) {
+    echo "Dados recebidos com sucesso!";
+    echo "<br>Nome: $nome";
+    echo "<br>Email: $email";
+
+  } else {
+    foreach( $validacoes as $validacoes) {
+      echo "$validacoes<br>";
+    }
+  }
+ } else {
+  echo "Requisição inválida.";
+ };
 ?>
