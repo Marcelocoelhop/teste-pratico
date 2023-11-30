1e4b6a5ab77a47c28a1ca98b1c68aae7b3ce510d
Mikaelpro1010 <mkpro10@alu.ufc.br>
2023-11-30 11:22:08 -0300

    Recuperando os dados enviados pelo formulário e exibindo uma mensagem na tela com os dados do usuário(nome e email)

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,18 @@
-<?php
-  // SEU CÓDIGO PHP AQUI
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    if(isset($_POST['enviar'])){
+      $nome = $_POST['name'];
+      $email = $_POST['email'];
+
+      echo "O nome do usuário é $nome e o seu email é $email";
+    }
+  ?>
+</body>
+</html>
