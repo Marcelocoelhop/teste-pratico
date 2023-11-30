fc22bbe01e71bd14f801b94aefe73009faa082ca
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2023-11-30 20:03:29 -0300

    Adicionando validação no backend

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,10 +1,23 @@
 <?php
   
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
+  $nome = isset($_POST['nome']) ? trim($_POST['nome']) : null;
+  $email = isset($_POST['email']) ? filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL) : null;
+  $msg = '';
 
-  echo "Nome: {$nome} <br> Email: {$email}. <br>";
+  if(empty($nome)){
+    $msg .= "Campo Nome não pode ser vazio. <br>";
+  }
 
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL )){
+    $msg .= "Campo Email vazio ou inválido.";
+  }
+   
+  if($msg == ''){
+    echo "Nome: {$nome} <br> Email: {$email} <br>";
+  }else{
+    echo $msg ."<br>";
+  }
+  
   echo "<a href='./formulario.html'>Voltar</a>";
 
 ?>
