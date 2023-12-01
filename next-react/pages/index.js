1096f49b0a99452fedf38453d12f11cb378c9985
Kevin Lucas <kevinbrito2012@gmail.com>
2023-12-01 11:18:03 -0300

    feat: implementa lógica de incremento/decremento

--- next-react/pages/index.js
+++ next-react/pages/index.js
@@ -3,13 +3,17 @@ import { useState } from "react";
 export default function Contador() {
   const [count, setCount] = useState(0);
 
-  const increment = () => {};
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-  const decrement = () => {};
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
   return (
     <div>
-      <h1>0</h1>
+      <h1>{count}</h1>
       <button
         type='button'
         onClick={increment}
