13f661e70f0812a6a7d9ada4ea8029a5e1456950
Kevin Lucas <kevinbrito2012@gmail.com>
2023-12-01 10:12:47 -0300

    feat: adiciona script PHP para processar dados

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,35 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    $erros = array();
+
+    if (empty($nome)) {
+      array_push($erros, "Preencha o campo nome.");
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      array_push($erros, "Insira um e-mail válido.");
+    }
+
+    if (empty($senha)) {
+      array_push($erros, "Preencha o campo senha.");
+    }
+
+    if (empty($erros)) {
+      echo "Formulário enviado com sucesso!<br>";
+      echo "Nome: " . htmlspecialchars($nome) . "<br>";
+      echo "E-mail: " . htmlspecialchars($email) . "<br>";
+      echo "Senha: " . htmlspecialchars(str_repeat("*", strlen($senha))) . "<br>";
+    } else {
+      echo "Erro ao enviar o formulário:<br>";
+      foreach ($erros as $erro) {
+        echo "- " . htmlspecialchars($erro) . "<br>";
+      }
+    }
+  } else {
+    echo "Erro ao enviar o formulário: Acesso inválido.";
+  }
 ?>
